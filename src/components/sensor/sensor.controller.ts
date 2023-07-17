7c7766bc617940f539f242a748bfa98445b721c3
reewaz401 <reewazmaskey@gmail.com>
2023-07-17 13:35:42 +0200

    push mqtt

--- src/components/sensor/sensor.controller.ts
+++ src/components/sensor/sensor.controller.ts
@@ -1,4 +1,46 @@
+import client from "../../utils/mqtt";
+
 class SensorController {
+
+
+  private static client: SensorController;
+
+  static getData() {
+    const topic = "<group>/packet/<gateway_id>/<node_id>/<sensor_id>";
+    SensorController.getconnection(topic);
+  }
+
+  static getconnection(topic: string) {
+
+    client.on('connect', function () {
+      console.log("Connected MQTT!");
+
+      // Subscribe to the topic
+      client.subscribe(topic, function (error) {
+        if (error) {
+          console.log("Error subscribing to topic:", error);
+        } else {
+          console.log("Subscribed to topic successfully!");
+        }
+      });
+    });
+
+    // Handle MQTT events and messages
+    client.on('message', function (topic, message) {
+      console.log("Received message on topic:", topic);
+      console.log("Message:", message.toString());
+    });
+
+    client.on('error', function (error) {
+      console.log("MQTT error:", error);
+    });
+
+    client.on('close', function () {
+      console.log("MQTT connection closed.");
+    });
+  }
+}
+
 
 }
 
