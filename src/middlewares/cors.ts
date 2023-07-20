e898e528a703e4ba8f7219a8983572e6ab7bc9b8
anonems <93661372+anonems@users.noreply.github.com>
2023-07-20 16:24:26 +0200

    update

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-
+const allowedOrigins = ['http://localhost:3000', 'https://iot-backend-ym14.onrender.com'];
+const options: cors.CorsOptions = {
+   origin: allowedOrigins
+ };
 class CORS {
+
+
    init(app: express.Application) {
-      app.use(cors());
+      app.use(cors(options));
    }
 }
 
