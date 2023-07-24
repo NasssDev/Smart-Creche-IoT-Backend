88f2cacac63bf5b2385ca6c0e8ca4f4ad378475f
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-07-24 14:16:36 +0200

    Update cors.ts

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 class CORS {
-
+const allowedOrigin = [
+         'http://localhost:5173',
+         'http://localhost:5174'
+      ];
 
    init(app: express.Application) {
       app.use(cors({
-         origin: "*",
+         origin: allowedOrigin,
          method: 'GET,HEAD,PUT,PATCH,POST,DELETE',
          credential:  true,
          exposedHeaders: ["*"]
