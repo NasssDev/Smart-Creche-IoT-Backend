c0f35f573dbf8b1b3055dacd858fab879af88029
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-08-09 01:04:54 +0200

    Update cors.ts

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -3,7 +3,8 @@ import cors from 'cors';
 class CORS {
 private allowedOrigin = [
          'http://localhost:5173',
-         'http://localhost:5174'
+         'http://localhost:5174',
+         'https://smart-creche.vercel.app'
       ];
 
    init(app: express.Application) {
